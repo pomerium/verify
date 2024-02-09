5a9159bd1881facf9d141e35989d8cb0840108b1
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2024-02-08 17:22:16 -0800

    ui: resolve a React warning
    Resolve a warning message about how "ReactDOM.render is no longer
    supported in React 18" according to the instructions at
    https://reactjs.org/link/switch-to-createroot.

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('main'));
+const root = createRoot(document.getElementById('main'));
+root.render(<App />);
