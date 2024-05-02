07e71f7536c40922560e29ce59ffb0efacc5025a
Joe Kralicky <joekralicky@gmail.com>
2024-05-02 11:52:41 -0400

    Add support for custom CA certs (#323)
    This adds a new environment variable 'EXTRA_CA_CERTS' which can contain
    a comma-separated list of file paths to CA certs. These certs will be
    used in addition to the system defaults.

--- cmd/verify/main.go
+++ cmd/verify/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"os"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 
@@ -27,12 +29,22 @@ func main() {
 		firestoreProjectID = v
 	}
 
+	var extraCaCerts []string
+	if v, ok := os.LookupEnv("EXTRA_CA_CERTS"); ok {
+		var err error
+		extraCaCerts, err = csv.NewReader(strings.NewReader(v)).Read()
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to parse $EXTRA_CA_CERTS (expected comma-separated list of file paths)")
+		}
+	}
+
 	srv := verify.New(
 		verify.WithBindAddress(addr),
 		verify.WithFirestoreProjectID(firestoreProjectID),
 		verify.WithJWKSEndpoint(jwksEndpoint),
 		verify.WithExpectedJWTIssuer(os.Getenv("EXPECTED_JWT_ISSUER")),
 		verify.WithExpectedJWTAudience(os.Getenv("EXPECTED_JWT_AUDIENCE")),
+		verify.WithExtraCACerts(extraCaCerts...),
 	)
 	err := srv.Run(context.Background())
 	if err != nil {
