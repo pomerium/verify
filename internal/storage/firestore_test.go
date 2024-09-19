6f23fa522a663b7fe54aedf62bff3059c06a435e
Caleb Doxsey <cdoxsey@pomerium.com>
2024-09-19 15:14:52 -0600

    verify/tests: use testcontainers

--- internal/storage/firestore_test.go
+++ internal/storage/firestore_test.go
@@ -2,21 +2,24 @@ package storage
 
 import (
 	"context"
-	"os"
-	"path/filepath"
+	"net"
 	"testing"
 	"time"
 
 	"cloud.google.com/go/firestore"
-	"github.com/ory/dockertest/v3"
-	"github.com/ory/dockertest/v3/docker"
-	"github.com/pomerium/webauthn"
-	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/pomerium/webauthn"
 )
 
 func TestFirestoreBackend(t *testing.T) {
-	assert.NoError(t, WithTestFirestore(func(ctx context.Context, client *firestore.Client) error {
+	ctx, clearTimeout := context.WithTimeout(context.Background(), time.Minute*10)
+	defer clearTimeout()
+
+	WithTestFirestore(t, func(client *firestore.Client) {
 		backend := NewFirestoreBackend(client)
 		err := backend.SetCredential(ctx, &webauthn.Credential{
 			ID: []byte{1, 2, 3, 4},
@@ -26,96 +29,46 @@ func TestFirestoreBackend(t *testing.T) {
 		cred, err := backend.GetCredential(ctx, []byte{1, 2, 3, 4})
 		assert.NoError(t, err)
 		assert.NotNil(t, cred)
-
-		return nil
-	}))
+	})
 }
 
-// WithTestFirestore starts a test firestore emulator.
-func WithTestFirestore(handler func(context.Context, *firestore.Client) error) error {
-	maxWait := time.Minute * 10
-
-	ctx, clearTimeout := context.WithTimeout(context.Background(), maxWait)
+func WithTestFirestore(t *testing.T, f func(client *firestore.Client)) {
+	ctx, clearTimeout := context.WithTimeout(context.Background(), time.Minute*10)
 	defer clearTimeout()
 
-	// if we've already set an emulator, don't start a new one
-	if _, ok := os.LookupEnv("FIRESTORE_EMULATOR_HOST"); ok {
-		client, err := firestore.NewClient(ctx, "test")
-		if err != nil {
-			log.Error().Err(err).Send()
-			return err
-		}
-		defer client.Close()
-		return handler(ctx, client)
-	}
-
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		return err
-	}
-
-	wd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-
-	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "andreysenov/firebase-tools",
-		Tag:        "latest",
-		Cmd: []string{
-			"firebase",
-			"emulators:start",
-			"--project",
-			"test",
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Name:         "pomerium-verify-firebase",
+			Image:        "andreysenov/firebase-tools:latest",
+			ExposedPorts: []string{"8080/tcp"},
+			Cmd: []string{
+				"firebase",
+				"emulators:start",
+				"--project",
+				"test",
+			},
+			WaitingFor: wait.ForAll(
+				wait.ForListeningPort("8080"),
+				wait.ForLog("Emulator Hub running"),
+			),
+			Files: []testcontainers.ContainerFile{{
+				HostFilePath:      "../../firebase.json",
+				ContainerFilePath: "/home/node/firebase.json",
+				FileMode:          0o644,
+			}},
 		},
-		Mounts: []string{
-			filepath.Join(wd, "..", "..") + ":/home/node",
-		},
-		ExposedPorts: []string{"8080"},
+		Started: true,
+		Logger:  testcontainers.TestLogger(t),
+		Reuse:   true,
 	})
-	if err != nil {
-		return err
-	}
-	_ = resource.Expire(uint(maxWait.Seconds()))
-	go tailLogs(ctx, pool, resource)
+	require.NoError(t, err)
 
-	host := resource.Container.NetworkSettings.IPAddress + ":8080"
+	mappedPort, err := container.MappedPort(ctx, "8080")
+	require.NoError(t, err)
 
-	defer func() { os.Unsetenv("FIRESTORE_EMULATOR_HOST") }()
-	os.Setenv("FIRESTORE_EMULATOR_HOST", host)
+	t.Setenv("FIRESTORE_EMULATOR_HOST", net.JoinHostPort("127.0.0.1", mappedPort.Port()))
+	client, err := firestore.NewClient(ctx, "test")
+	require.NoError(t, err)
 
-	var client *firestore.Client
-	if err := pool.Retry(func() error {
-		log.Info().Str("host", host).Msg("connecting to firestore")
-		var err error
-		client, err = firestore.NewClient(ctx, "test")
-		if err != nil {
-			log.Error().Err(err).Send()
-			return err
-		}
-		return nil
-	}); err != nil {
-		_ = pool.Purge(resource)
-		return err
-	}
-
-	e := handler(ctx, client)
-	_ = client.Close()
-
-	if err := pool.Purge(resource); err != nil {
-		return err
-	}
-
-	return e
-}
-
-func tailLogs(ctx context.Context, pool *dockertest.Pool, resource *dockertest.Resource) {
-	_ = pool.Client.Logs(docker.LogsOptions{
-		Context:      ctx,
-		Stderr:       true,
-		Stdout:       true,
-		Follow:       true,
-		Container:    resource.Container.ID,
-		OutputStream: os.Stderr,
-	})
+	f(client)
 }
